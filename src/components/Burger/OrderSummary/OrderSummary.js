99320bd050da7393a973a09c44b53a7c91cde6fa
Uğur Eliaçık <ugur.eliacik@euromsg.com>
2018-07-18 15:53:00 +0300

    Lecture 139

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,7 @@ return (
         <ul>
             {ingredientSummary}
         </ul>
+        <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
         <p>Continue to Cheokout?</p>
         <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
         <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Legio from '../../hoc/Legio';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = (props) => (
-    <Legio>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Legio>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState)=>{
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+    render() {
+        return (
+            <Legio>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Legio>
+        );
+    }
+};
 
-export default layout;
+export default Layout;
